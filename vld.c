d35d8adb9f31175aac95a1b382ffcb8afbb51812
Derick Rethans <github@derickrethans.nl>
2012-03-09 23:12:27 +0000

    Back to -dev.

--- vld.c
+++ vld.c
@@ -64,7 +64,7 @@ zend_module_entry vld_module_entry = {
 	PHP_RSHUTDOWN(vld),
 	PHP_MINFO(vld),
 #if ZEND_MODULE_API_NO >= 20010901
-	"0.11.1",
+	"0.12.0-dev",
 #endif
 	STANDARD_MODULE_PROPERTIES
 };
