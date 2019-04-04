7d89e8c495048d8a7eb32b4606274b687b9f640b
Derick Rethans <github@derickrethans.nl>
2019-04-04 10:29:12 +0100

    Update rebuild script

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+FORCE=${1:-"YES"}
+
+if [[ ${FORCE} == "YES" ]]; then
+	echo "Forcing builds for all versions"
+fi
+
 for i in /usr/local/php/*; do
 	version=`echo $i | sed 's@.*/@@'`
 	echo -n "Rebuilding for PHP ${version}: "
 
-    export PATH=/usr/local/php/${version}/bin:/usr/lib/ccache:/usr/local/bin:/usr/bin:/bin
+	FILE=/usr/local/php/${version}/lib/php/extensions/*/vld.so
 
-	./rebuild.sh >/tmp/vld-for-${version}.log 2>&1
+	if [ ! -e $FILE ] || [[ ${FORCE} == "YES" ]]; then
+		export PATH=/usr/local/php/${version}/bin:/usr/lib/ccache:/usr/local/bin:/usr/bin:/bin
 
-	if [[ $? == 0 ]]; then 
-		echo "DONE"
+		LOG="/tmp/vld-for-${version}.log"
+		echo -n "Building: "
+		./rebuild.sh >$LOG 2>&1
+
+		if [[ $? == 0 ]]; then
+			echo "DONE"
+		else
+			echo "FAIL, log is at $LOG"
+		fi
 	else
-		echo "FAIL"
+		echo "SKIP"
 	fi
 done
--- rebuild.sh
+++ rebuild.sh
@@ -5,5 +5,4 @@ bit64=`php -n -r 'echo PHP_INT_SIZE == 8 ? "1" : "0";'`
 if [[ ${bit64} != "1" ]]; then
 	export CFLAGS="-m32"
 fi
-
 phpize && ./configure --enable-vld-dev && make clean && make all && make install
