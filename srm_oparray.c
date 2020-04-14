4328ee0685049588fed91d0342c9944b1f929bc9
jimmy.li <jimmy.li@ambersec.com>
2020-04-14 14:43:41 +0800

    When the code line is over 10000, the lineno column is not shown correctly.

--- srm_oparray.c
+++ srm_oparray.c
@@ -762,7 +762,7 @@ void vld_dump_op(int nr, zend_op * op_ptr, unsigned int base_address, int notdea
 	if (op.lineno == last_lineno) {
 		vld_printf(stderr, "     ");
 	} else {
-		vld_printf(stderr, "%4d ", op.lineno);
+		vld_printf(stderr, "%5d ", op.lineno);
 		last_lineno = op.lineno;
 	}
 
